ba7bd5ecb050d3a5a1eefbaa31738d99b95bd2b5
Amadus <790494168@qq.com>
2023-04-03 22:53:02 -0700

    update appending tweet to feeds, append to front

--- src/main/java/my/service/dao/RedisDAO.java
+++ src/main/java/my/service/dao/RedisDAO.java
@@ -1,12 +1,15 @@
 package my.service.dao;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import my.service.dto.FeedDTO;
 import my.service.dto.TweetDTO;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class RedisDAO {
     private final JedisPool jedisPool;
@@ -30,37 +33,22 @@ public class RedisDAO {
     public void addTweetToFeed(String userID, TweetDTO tweet) {
         try (Jedis jedis = jedisPool.getResource()) {
             Gson gson = new Gson();
-            String feedJson = jedis.get(userID);
-            if (feedJson == null) {
-                FeedDTO feedDTO = new FeedDTO();
-                feedDTO.setUserID(userID);
-                feedDTO.setTweetsList(new ArrayList<>());
-                feedDTO.getTweetsList().add(tweet);
-                String newFeedJson = gson.toJson(feedDTO);
-                jedis.set(userID, newFeedJson);
-            }
-            else {
-                FeedDTO oldDTO = gson.fromJson(feedJson, FeedDTO.class);
-                oldDTO.getTweetsList().add(tweet);
-                String newFeedJson = gson.toJson(oldDTO);
-                jedis.set(userID, newFeedJson);
-            }
+            String key = "feeds-" + userID;
+            jedis.lpush(key, gson.toJson(tweet));
         }
     }
 
-    public FeedDTO getFeed(String userID) {
+    public FeedDTO getFeed(String userID) throws Exception {
         try (Jedis jedis = jedisPool.getResource()) {
             Gson gson = new Gson();
-            String feedJson = jedis.get(userID);
-            if (feedJson == null) {
-                FeedDTO emptyDTO = new FeedDTO();
-                emptyDTO.setUserID(userID);
-                emptyDTO.setTweetsList(new ArrayList<>());
-                return emptyDTO;
-            }
-            else {
-                return gson.fromJson(feedJson, FeedDTO.class);
-            }
+            String key = "feeds-" + userID;
+            List<String> tweetJsonList = jedis.lrange(key, 0, -1);
+            Type listType = new TypeToken<List<TweetDTO>>() {}.getType();
+            List<TweetDTO> tweets = gson.fromJson(tweetJsonList.toString(), listType);
+            FeedDTO feed = new FeedDTO();
+            feed.setUserID(userID);
+            feed.setTweetsList(tweets != null ? tweets : new ArrayList<>());
+            return feed;
         }
     }
 }
