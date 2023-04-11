58d2a20790c174293fb1bcc5cb8eda6181e571f3
David Yang <zyang03@risd.edu>
2023-04-11 16:05:33 -0700

    switch to dax configuration

--- src/main/java/my/service/config/DynamoDBConfig.java
+++ src/main/java/my/service/config/DynamoDBConfig.java
@@ -1,8 +1,9 @@
 package my.service.config;
-
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.dax.AmazonDaxClient;
+import com.amazonaws.services.dax.AmazonDaxClientBuilder;
+// import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+// import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @EnableDynamoDBRepositories(basePackages = "my.service.repository")
 public class DynamoDBConfig {
     @Bean
-    public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://34.213.26.95:8000", "us-west-2"))
-                .build();
+    public AmazonDaxClient amazonDynamoDB() {
+        // return AmazonDynamoDBClientBuilder.standard()
+        //         .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://34.213.26.95:8000", "us-west-2"))
+        //         .build();
+
+        // Create a DAX client object
+        return (AmazonDaxClient) AmazonDaxClientBuilder.standard()
+            .withEndpointConfiguration(new EndpointConfiguration("dax://dax.iomfja.dax-clusters.us-west-2.amazonaws.com", "us-west-2"))
+            .build();
     }
 }
