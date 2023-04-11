87c396edc2b2c35b11f509a86b9c6a6d40b9d2bd
David Yang <zyang03@risd.edu>
2023-04-11 16:54:50 -0700

    finish the dax config

--- src/main/java/my/service/config/DynamoDBConfig.java
+++ src/main/java/my/service/config/DynamoDBConfig.java
@@ -1,7 +1,7 @@
 package my.service.config;
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
-import com.amazonaws.services.dax.AmazonDaxClient;
-import com.amazonaws.services.dax.AmazonDaxClientBuilder;
+import com.amazon.dax.client.dynamodbv2.AmazonDaxClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+
 // import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 // import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
@@ -12,14 +12,18 @@ import org.springframework.context.annotation.Configuration;
 @EnableDynamoDBRepositories(basePackages = "my.service.repository")
 public class DynamoDBConfig {
     @Bean
-    public AmazonDaxClient amazonDynamoDB() {
+    public AmazonDynamoDB amazonDynamoDB() {
         // return AmazonDynamoDBClientBuilder.standard()
         //         .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://34.213.26.95:8000", "us-west-2"))
         //         .build();
 
         // Create a DAX client object
-        return (AmazonDaxClient) AmazonDaxClientBuilder.standard()
-            .withEndpointConfiguration(new EndpointConfiguration("dax://dax.iomfja.dax-clusters.us-west-2.amazonaws.com", "us-west-2"))
-            .build();
+        // return (AmazonDaxClient) AmazonDaxClientBuilder.standard()
+        //     .withEndpointConfiguration(new EndpointConfiguration("dax://dax.iomfja.dax-clusters.us-west-2.amazonaws.com", "us-west-2"))
+        //     .build();
+        AmazonDaxClientBuilder daxClientBuilder = AmazonDaxClientBuilder.standard();
+        daxClientBuilder.withEndpointConfiguration("dax.iomfja.dax-clusters.us-west-2.amazonaws.com:8111");
+        AmazonDynamoDB client = daxClientBuilder.build();
+        return client;
     }
 }
